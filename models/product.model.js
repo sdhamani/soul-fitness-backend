29e87f62351231a89a41a7e3404065e93fb164cb
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-04-28 05:22:22 +0530

    Added Image to the model

--- models/product.model.js
+++ models/product.model.js
@@ -11,6 +11,10 @@ const ProductSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    image: {
+      type: String,
+      required: true,
+    },
     price: {
       type: Number,
       required: [true, "Why no price?"],
